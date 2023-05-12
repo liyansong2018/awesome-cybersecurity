6ee93feaec885e838a053d6bc4fd5ec05baf3ad0
liyansong2018 <1321993383@qq.com>
2023-05-12 17:35:04 +0800

    Upgrade to v1.1 caused by new github api

--- reptile_security.py
+++ reptile_security.py
@@ -1,4 +1,8 @@
 '''
+Author: liyansong2018
+Version: 1.1
+Date: 2023-05-12
+Description: github modify json api
 Caution: You have exceeded a secondary rate limit.
 - User-to-server requests are limited to 5,000 requests per hour and per authenticated user.
 - For unauthenticated requests, the rate limit allows for up to 60 requests per hour.
@@ -9,6 +13,7 @@ https://docs.github.com/en/developers/apps/building-github-apps/rate-limits-for-
 # -*- coding: UTF-8 -*-
 
 import requests
+import urllib3
 from bs4 import BeautifulSoup
 import json
 import re
@@ -30,20 +35,6 @@ class Url:
 
     def get_repo(self):
         return self.ret[3]
-
-
-def get_page(url, headers):
-    """
-    Get page number
-    :param url: search url
-    :param headers: requests headers
-    :return: page number
-    """
-    response = requests.get(url=url, headers=headers, verify=False)
-    soup = BeautifulSoup(response.text, 'lxml')
-    page_sel = soup.select('#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > div.paginate-container.codesearch-pagination-container > div > em')
-    return int(page_sel[0]['data-total-pages'])
-
 
 def handle_star(github_star):
     """
@@ -84,26 +75,25 @@ def handle_disc(github_disc):
     :param github_disc:
     :return:
     """
-    pattern = r'[\w| |.|-]+'
-    disc = re.findall(pattern, github_disc)
-    return disc[0].strip()
+    if github_disc != None:
+        # ...->…
+        pattern = r'(\<.*?\>)|…'
+        disc = re.sub(pattern, '', github_disc)
+        return disc.strip()
+    else:
+        return github_disc
 
-def main():
+def main(keywords, page_number, star_number, cookie, proxies=None):
     if len(sys.argv) <= 1:
         print('<usage>: %s [file.csv]')
         exit(-1)
-
-    proxies = {
-        "http": "http://localhost:1080",
-        "https": "https://localhost:1080",
-    }
 
     headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'Accept-encoding': 'gzip, deflate, br',
         'Accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
         'Cache-control': 'max-age=0',
-        'Cookie': '',       # Your cookie
+        'Cookie': cookie,       # Your cookie
         'if-none-match': 'W/"8c869a44ef95afccba0e45886711dbbf"',
         'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="98", "Microsoft Edge";v="98"',
         'sec-ch-ua-mobile': '?0',
@@ -117,64 +107,62 @@ def main():
     }
 
     repo = []
-    url = 'https://github.com/search?q=awesome-security'
-    page = get_page(url, headers)
-    for i in range(20):
-        url = 'https://github.com/search?p={}&q=awesome-security&type=Repositories'.format(i + 1)
-        # github-error-rate-limit-exceeded
-        response = requests.get(url=url, headers=headers, verify=False)
-        soup = BeautifulSoup(response.text, 'lxml')
-        date = soup.findAll('relative-time')
-        j = 0
-        while True:
-            j += 1
-            proj = soup.select('#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-of-type({}) > div.mt-n1.flex-auto > div.d-flex > div > a'.format(j))
-            disc = soup.select('#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-of-type({}) > div.mt-n1.flex-auto > p'.format(j))
-            if proj == None or len(proj) == 0:
+    has_id = []
+    for keyword in keywords:
+        for i in range(page_number):
+            url = 'https://github.com/search?p={}&q={}&type=Repositories'.format(i + 1, keyword)
+            # github-error-rate-limit-exceeded
+            response = requests.get(url=url, headers=headers, proxies=proxies, verify=False)
+            soup = BeautifulSoup(response.text, 'lxml')
+            application_json = soup.find_all('script', attrs={'type': 'application/json', 'data-target': 'react-app.embeddedData'})[0]
+            dict = json.loads(application_json.get_text())
+            if dict['payload']['results'] == None:
                 break
 
-            print(proj)
-            data = json.loads(proj[0]['data-hydro-click'])
-            repo_url = data['payload']['result']['url']
-            url = Url(repo_url)
-            repo_name = url.get_repo()
-            repo_user = url.get_user()
-            if disc != None and len(disc) > 0:
-                repo_disc = disc[0].text.strip()
-                star = soup.select(
-                    '#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-of-type({}) > div.mt-n1.flex-auto > div:nth-child(3) > div.d-flex.flex-wrap.text-small.color-fg-muted > div:nth-child(1) > a'.format(
-                        j))
+            for item in dict['payload']['results']:
+                print(item)
+                id = item['id']
+                name = item['repo']['repository']['name']
+                user = item['repo']['repository']['owner_login']
+                discription = item['hl_trunc_description']
+                star = item['followers']
+                update_time = item['repo']['repository']['updated_at']
+                year = float(update_time[:4])
+                repo_url = 'https://github.com/{}/{}'.format(user, name)
 
-            else:
-                repo_disc = "NULL"
-                star = soup.select(
-                    '#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-of-type({}) > div.mt-n1.flex-auto > div:nth-child(2) > div.d-flex.flex-wrap.text-small.color-fg-muted > div:nth-child(1) > a'.format(
-                        j))
+                if id not in has_id and star >= star_number:
+                    has_id.append(id)
+                else:
+                    break
 
-            if star != None and len(star) > 0:
-                repo_star = star[0].text.strip()
-            else:
-                repo_star = '0'
+                repo.append(
+                    {
+                        'id': id,
+                        'name': name,
+                        'user': user,
+                        'discription': handle_disc(discription),
+                        'star': star,
+                        'update time': update_time,
+                        'year': year,
+                        'url': repo_url
+                    })
 
-            if date != None and len(date) > 0:
-                repo_date = date[j - 1]['datetime']
-
-            repo.append(
-                {
-                    'name': repo_name,
-                    'user': repo_user,
-                    'discription': handle_disc(repo_disc),
-                    'star': handle_star(repo_star),
-                    'update time': repo_date,
-                    'distance': float(handle_date(repo_date)['year']),
-                    'url': repo_url
-                })
-
-    head = ['name', 'user', 'discription', 'star', 'update time', 'distance', 'url']
+    head = ['id', 'name', 'user', 'discription', 'star', 'update time', 'year', 'url']
     with open(sys.argv[1], 'w', newline='', encoding='utf-8') as fp:
         fp_csv = csv.DictWriter(fp, head)
         fp_csv.writeheader()
         fp_csv.writerows(repo)
 
 if __name__ == '__main__':
-    main()
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    cookie = '' # Your github account cookie!
+    proxies = {
+        "http": "http://localhost:10809",
+        "https": "http://localhost:10809",
+    }
+    keywords = ['awesome-security', 'awesome_cybersecurity']
+    page_number = 20
+    star_number = 10
+
+    main(keywords, page_number, star_number, cookie, proxies)
+
